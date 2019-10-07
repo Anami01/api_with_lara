80a7e5d8ea03d6ed4594a754ea4f858991a22041
Chahil <chahil@topdoc.ai>
2019-10-07 11:41:06 +0530

    changes

--- database/migrations/2019_08_30_115510_create_users_table.php
+++ database/migrations/2019_08_30_115510_create_users_table.php
@@ -25,7 +25,6 @@ class CreateUsersTable extends Migration
             $table->string('password')->nullable($value = true);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-            $table->string('api_token', 60)->unique()->nullable($value = true);
         });
     }
 
